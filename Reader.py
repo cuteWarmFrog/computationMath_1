b5398244cc3a637cee707c0c019e303d18700c3c
cuteWarmFrog <livshits.victor@gmail.com>
2021-04-14 00:11:03 +0300

    added console and fileReader

---
+++ Reader.py
@@ -0,0 +1,24 @@
+def readMatrix(consoleOrFile):
+    if consoleOrFile == '1':
+        return readFromConsole()
+    elif consoleOrFile == '2':
+        return readFromFile()
+    else:
+        print('No i am going to die.')
+        return None
+
+
+def readFromConsole():
+    matrix = []
+    n = int(input('Какая размерность у матрицы?'))
+    for i in range(n):
+        matrix.append(map(lambda x: int(x), input().split(' ')))
+    return matrix
+
+
+def readFromFile():
+    matrix = []
+    f = open('test.txt', 'r')
+    for line in f:
+        matrix.append(list(map(lambda x: int(x), line.split(' '))))
+    return matrix
--- index.py
+++ index.py
@@ -1,10 +1,19 @@
 import numpy as np
+import Reader
 
 
 def greet():
     print('Приветствую тебя, странник.\nЧувствуй себя как дома, но не забывай, что ты в гостях.\n')
 
 
-if __name__ == '__main__':
-    greet()
+greet()
+answer = input('Напиши (1), если хочешь ввести матрицы вручную или (2), если из файла.\n')
 
+matrix = Reader.readMatrix(answer)
+
+if matrix is not None:
+    print('Ваша матрица:')
+    for line in matrix:
+        print(line)
+else:
+    print('Вы запостили кринж, весь город должен умереть!')
