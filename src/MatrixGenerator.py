227192b4f93229e5d89a6da7018b08c2fcb5054f
cuteWarmFrog <livshits.victor@gmail.com>
2021-04-19 21:34:59 +0300

    added Diagonal predominance generation

--- src/MatrixGenerator.py
+++ src/MatrixGenerator.py
@@ -1,9 +1,20 @@
 import numpy as np
+from Seidel import checkDiagonal
 
-
-def generateMatrix(n):
-    matrix = np.random.rand(n, n + 1)
+# actually this sucks a lot. Too many shit words like lambda or list. JS is better in one string programs))
+def generateMatrixMap(n):
+    matrix = list(map(lambda line: list(map(lambda el: round(el * 10), line)), list(np.random.rand(n, n + 1))))
     for i in range(n):
-        for j in range(n + 1):
-            matrix[i][j] = round(matrix[i][j] * 10)
+        matrix[i][i] = matrix[i][i] * 100
     return matrix
+
+
+def generateMatrixWithDiagonal(n):
+    matrix = generateMatrixMap(n)
+    while checkDiagonal(matrix) != 1:
+        matrix = generateMatrixMap(n)
+
+
+# this looks better but does not work lol todo make this shit work
+def generateMatrixCool(n):
+    return list([round(el * 10) for el in [line for line in [np.random.rand(n, n + 1)]]])
--- src/Tester.py
+++ src/Tester.py
@@ -1,17 +1,24 @@
-from MatrixGenerator import generateMatrix
-from Seidel import Seidel
+import MatrixGenerator
+from Seidel import Solve
 
 
 def testOnRandomMatrices(n, accuracy):
+    norm = 0
+    nenorm = 0
     for i in range(n):
         print(str(i) + " test \n")
-        matrix = generateMatrix(n)
-        answer, iterations = Seidel(matrix, accuracy)
-        if answer:
-            print('Итараций:', iterations)
-            print('Ответ:', answer)
+        matrix = MatrixGenerator.generateMatrixMap(4)
+        answer, iterations = Solve(matrix, accuracy)
+        if len(answer) > 0:
+            norm += 1
+            # print('Итараций:', iterations)
+            # print('Ответ:', answer)
         else:
-            print("Достингуто пиковое число итераций. Итерации расходятся :(")
+            nenorm += 1
+            # print("Достингуто пиковое число итераций. Итерации расходятся :(")
+
+    print(norm)
+    print(nenorm)
 
 
-testOnRandomMatrices(8, 0.001)
+testOnRandomMatrices(100000, 0.01)
